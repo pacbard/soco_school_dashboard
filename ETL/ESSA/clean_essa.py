5d88cd331fc0ba9f689d38e22a120ec89e6c3dcd
Emanuele Bardelli <bardellie@gmail.com>
2025-01-03 09:47:10 -0800

    Add ESSA data download link

--- ETL/ESSA/clean_essa.py
+++ ETL/ESSA/clean_essa.py
@@ -1,4 +1,8 @@
 import pandas as pd
+import urllib.request
+
+# Download the excel spreadsheet from CDE website: https://www.cde.ca.gov/sp/sw/t1/essaassistdatafiles.asp
+urllib.request.urlretrieve("https://www.cde.ca.gov/sp/sw/t1/documents/essaassistance24.xlsx", "essaassistance24.xlsx")
 
 # Read the Excel file into a pandas DataFrame
 df = pd.read_excel('essaassistance24.xlsx', sheet_name='2024-25 ESSA State Schools', header=None) 
