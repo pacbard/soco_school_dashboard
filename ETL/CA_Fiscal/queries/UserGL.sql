4bf381645f63e785439226426a32ac8ea6668aa4
Emanuele Bardelli <bardellie@gmail.com>
2025-04-06 12:32:10 -0700

    Remove order by from ledger queries
    Ordering the parquet file results in an OOM error

--- ETL/CA_Fiscal/queries/UserGL.sql
+++ ETL/CA_Fiscal/queries/UserGL.sql
@@ -1,6 +1,8 @@
 -- General Ledger
 load https;
 
+SET preserve_insertion_order = false;
+
 copy (
   select
     Ccode::int as CCcode,
@@ -19,5 +21,5 @@ copy (
     cast(Value as decimal(15, 2)) as Value
   from read_parquet('data/sacs/processed/parquet/UserGL/*.parquet', 
     union_by_name=True)
-  order by CCcode, Dcode, Scode, FiscalYear, Account, Fund, Resource, Projectyear, Goal, Function
-) to 'data/UserGL.parquet' (FORMAT parquet, COMPRESSION zstd, ROW_GROUP_SIZE 100_000)
+  -- order by CCcode, Dcode, Scode, FiscalYear, Account, Fund, Resource, Projectyear, Goal, Function
+) to 'data/UserGL.parquet' (FORMAT parquet, COMPRESSION zstd, ROW_GROUP_SIZE 100_000);
--- ETL/CA_Fiscal/queries/UserGLTotals.sql
+++ ETL/CA_Fiscal/queries/UserGLTotals.sql
@@ -1,6 +1,8 @@
 -- General Ledger Totals
 load https;
 
+SET preserve_insertion_order = false;
+
 copy (
   select
     Ccode::int as CCcode,
@@ -18,5 +20,5 @@ copy (
     cast(Value as decimal(15, 2)) as Value
   from read_parquet('data/sacs/processed/parquet/UserGL_Totals/*.parquet', 
     union_by_name=True)
-  order by CCcode, Dcode, FiscalYear, Account, Fund, Resource, Projectyear, Goal, Function
+  -- order by CCcode, Dcode, FiscalYear, Account, Fund, Resource, Projectyear, Goal, Function
 ) to 'data/UserGLTotals.parquet' (FORMAT parquet, COMPRESSION zstd, ROW_GROUP_SIZE 100_000)
